66e1c4b60d260047e11ed746e888caa69ca763d1
popolino <polinadorina888@gmail.com>
2025-05-05 02:18:03 +0300

    meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,11 +4,14 @@ export default function Document() {
     return (
         <Html lang="ru">
             <Head>
-                <meta name="description" content="Weather App Test Work" />
+                <meta name="description" content="Weather App Test Work"/>
+                <meta property="og:type" content="website"/>
+                <meta property="og:title" content="TestWork77"/>
+                <link rel="icon" href="/favicon.ico"/>
             </Head>
             <body>
-            <Main />
-            <NextScript />
+            <Main/>
+            <NextScript/>
             </body>
         </Html>
     )
